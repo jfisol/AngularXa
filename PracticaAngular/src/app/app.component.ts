005e546451d30f1895f1b51d0c51ef3b3c627b1b
Julio <taodejoju@gmail.com>
2023-07-17 15:05:37 -0600

    tarea

--- PracticaAngular/src/app/app.component.ts
+++ PracticaAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Producto } from './interfaz/datos';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PracticaAngular';
+  
+  lprocucto : Producto[];
+  constructor(){
+    this.lprocucto =[
+      {nombres:'Laptop Acer',costos:3500},
+      {nombres:'Laptop Dell',costos:6500},
+      {nombres:'todo en uno Acer',costos:3500},
+      {nombres:'todo en uno Dell',costos: 4500},
+      {nombres:'Camaras de Seguridad', costos: 1500},
+      {nombres:'monitor upt', costos: 1500},
+      {nombres:'teclado ',costos: 1200},
+      {nombres: 'Adaptadores',costos:680},
+      {nombres:'Cargadores',costos: 650}
+    ]
+  }
 }
--- PracticaAngular/src/app/app.module.ts
+++ PracticaAngular/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { ListaComponent } from './lista/lista.component';
+import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
